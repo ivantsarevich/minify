88e309c137371406fe6ad964e7e7ef5297e05e80
Ivan E. <iamivanefremenkov@gmail.com>
2025-01-17 01:26:58 +0300

    Initialize Minify

---
+++ src/main/java/efr/iv/igr/minify/MinifyApplication.java
@@ -0,0 +1,13 @@
+package efr.iv.igr.minify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinifyApplication.class, args);
+    }
+
+}
